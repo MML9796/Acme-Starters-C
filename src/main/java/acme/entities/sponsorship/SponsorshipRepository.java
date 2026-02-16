4b5b804c1a388937ddca59fd6c91a21f060cd900
MML9796 <sansilama@alum.us.es>
2026-02-16 13:11:40 +0100

    Arreglo en el repositorio

--- src/main/java/acme/entities/sponsorship/SponsorshipRepository.java
+++ src/main/java/acme/entities/sponsorship/SponsorshipRepository.java
@@ -2,6 +2,7 @@
 package acme.entities.sponsorship;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import acme.client.repositories.AbstractRepository;
@@ -10,5 +11,5 @@ import acme.client.repositories.AbstractRepository;
 public interface SponsorshipRepository extends AbstractRepository {
 
 	@Query("SELECT SUM(d.money.amount) from Donation d where d.sponsorship.id = :id")
-	Double getSumTotalMoneyBySponsorship(int sponsorshipId);
+	Double getSumTotalMoneyBySponsorship(@Param("id") int sponsorshipId);
 }
