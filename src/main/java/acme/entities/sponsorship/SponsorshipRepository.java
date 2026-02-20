154f6744ae772b8f912f68a7b8e6217bf7cc52c1
SXD9555 <nurmarsan@alum.us.es>
2026-02-20 23:15:36 +0100

    Añadidas las restricciones a Model-C04 #22

--- src/main/java/acme/entities/sponsorship/SponsorshipRepository.java
+++ src/main/java/acme/entities/sponsorship/SponsorshipRepository.java
@@ -1,15 +1,21 @@
 
 package acme.entities.sponsorship;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import acme.client.repositories.AbstractRepository;
+import acme.entities.donation.Donation;
 
 @Repository
 public interface SponsorshipRepository extends AbstractRepository {
 
 	@Query("SELECT SUM(d.money.amount) from Donation d where d.sponsorship.id = :id")
 	Double getSumTotalMoneyBySponsorship(@Param("id") int sponsorshipId);
+
+	@Query("SELECT d FROM Donation d WHERE d.sponsorship.id = :sponsorshipId")
+	List<Donation> findDonationBySponsorshipId(int sponsorshipId);
 }
