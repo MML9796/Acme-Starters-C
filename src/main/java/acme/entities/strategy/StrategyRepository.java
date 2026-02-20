bafa700074004295e625d78d73dd96411550c002
MFC1728 <juagompoz@alum.us.es>
2026-02-20 11:05:38 +0100

    Custom constraints Model-C03 (#20)

--- src/main/java/acme/entities/strategy/StrategyRepository.java
+++ src/main/java/acme/entities/strategy/StrategyRepository.java
@@ -11,4 +11,7 @@ public interface StrategyRepository extends AbstractRepository {
 
 	@Query("select sum(t.expectedPercentage) from Tactic t where t.strategy.id = :id")
 	Double sumPercentageByStrategyId(int id);
+
+	@Query("select count(t) from Tactic t where t.strategy.id = :strategyId")
+	Integer countTacticsByStrategyId(int strategyId);
 }
